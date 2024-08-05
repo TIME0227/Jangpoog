9ce9552cb39e54e4064cdc8e124827f5e53d11d2
rnrgll <shuajjjjj@naver.com>
2024-08-05 16:50:35 +0900

    플레이어, 카메라 시작 위치, 이동 제한 구현, 1-2 맵만 임시 적용

--- Assets/Scripts/Common/CameraFollow.cs
+++ Assets/Scripts/Common/CameraFollow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    private StageData stageData;
     [SerializeField] private Transform target; //카메라 추적 대상
     [SerializeField] bool x = true, y = true, z;  //추적 축
     float offsetY; //target과 카메라의 y 거리 값을 저장하는 변수
@@ -12,17 +13,22 @@ public class CameraFollow : MonoBehaviour
 
     private void Awake()
     {
-        //target 자동으로 player로 설정
-        target = GameObject.FindWithTag("Player").transform;
-
+        if (target == null)
+        {
+            //target 자동으로 player로 설정
+            target = GameObject.FindWithTag("Player").transform;
+        }
         //offsetY 값 설정
         //target과 카메라의 y 거리 = |(카메라의 y 위치) - (tartget의 y 위치)|
         offsetY = Mathf.Abs(transform.position.y - target.position.y);
     }
-    void Start()
-    {
 
+    public void SetUp(StageData stageData)
+    {
+        this.stageData = stageData;
+        transform.position = new Vector3(stageData.CameraPosition.x, stageData.CameraPosition.y, -10);
     }
+
     private void LateUpdate()
     {
         if (target == null) return;
@@ -30,9 +36,12 @@ public class CameraFollow : MonoBehaviour
         //카메라의 위치는 x,y,z 각각 true면 target의 위치로, false면 카메라 위치 그대로
         //단 y 축은 target + offsetY
         transform.position = new Vector3(x ? target.position.x : transform.position.x, y ? target.position.y + offsetY : transform.position.y, z ? target.position.z : transform.position.z);
+        
+        //카메라의 좌/우측 이동 범위를 넘어가지 않도록 설정
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(transform.position.x, stageData.CameraLimitMinX, stageData.CameraLimitMaxX);
+        transform.position = position;
 
-
-        //카메라의 좌/우측 이동 범위를 넘어가지 않도록 설정(추가 예정)
 
     }
 }
