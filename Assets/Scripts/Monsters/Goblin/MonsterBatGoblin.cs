ad8d84c4edf514b76ff5697cebaa7d140d66f974
rnrgll <shuajjjjj@naver.com>
2024-08-23 08:16:31 +0900

    고블린 스탯 데이터, 프리팹 생성

--- Assets/Scripts/Monsters/Goblin/MonsterBatGoblin.cs
+++ Assets/Scripts/Monsters/Goblin/MonsterBatGoblin.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class MonsterBatGoblin : Monster
 {
     [SerializeField] private Transform throwStartPosition;
+    [SerializeField] private int batVersion = 1;
+    
     
     public void ThrowBat()
     {
-        Managers.Resource.Instantiate("Monsters/Goblin/Goblin_Bat", null, throwStartPosition.position);
+        Managers.Resource.Instantiate($"Monsters/Goblin/Goblin_Bat_v{batVersion}", null, throwStartPosition.position);
     }
 }
